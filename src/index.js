83bc39ad1ad161b2656fd9cbf01925aeacdec522
Adrian Insua <adrian.insua.y@gmail.com>
2021-03-12 01:58:19 +0100

    feat(study): add study component (#13)
    We modify header tag styles too in this commit
    
    Co-authored-by: adrianiy <adrianiy@ext.inditex.com>

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
+import { Provider } from "react-redux";
+import store from "./global/store/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
